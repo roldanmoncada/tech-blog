9e4af2d6407c99f5b712e4e7452df43fff2c6edb
roldanmoncada <roldan.h.moncada@gmail.com>
2022-12-22 19:01:22 -0700

    created the get all HTTPS route in dash-routes. A .js file that I will have to rename home-routes since I'll need to create an entire file for the dashboard routes.

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -1,7 +1,39 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.render("homepage");
+  try {
+    const dbPostData = await Post.findAll({
+      attributes: ["id", "title", "created_at", "post_content"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get("/login", (req, res) => {
