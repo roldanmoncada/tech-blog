bd7d89e3b9a2db11b332000351e5f2fdc3b29d23
roldanmoncada <roldan.h.moncada@gmail.com>
2023-02-05 19:02:28 -0700

    onto the public and views folders.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,21 +1,42 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
-const { post } = require('./dash-routes');
 
 router.get('/', async (req, res) => {
     try {
-        const post = await Post.findAll({
+        const postData = await Post.findAll({
             include: [
                 User
             ],
         });
         
-    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
     res.render('homepage', {posts})
     } catch (error) {
         res.status(500).json(error)
     }
 });
+
+router.get('/post/:id', async (req,res) => {
+    try {
+        const singlePost = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User],
+                }
+            ],
+        })
+        if (singlePost) {
+            const post = singlePost.get({ plain: true })
+            res.render('single-post', { post })
+        } else {
+            res.status(404).end()
+        }
+    } catch (error) {
+        res.status(500).end();
+    }
+})
 
 router.get("/login", (req, res) => {
     if (req.session.loggedIn){
--- public/login.js
+++ public/login.js
@@ -1,7 +1,8 @@
 const loginFormHandler = async function(event) {
     event.preventDefault();
   
-    const usernameEl = document.querySelector("#username-input-login");
+    const usernameEl = document.querySelector("#username-input-login"); 
+    // ^^ 'input[name="post-id"]' might be the necessary parameter passed into the querySelector. Currently think it's only for a new post and for a comment. Worth a google. could just be a handlebars thing. 
     const passwordEl = document.querySelector("#password-input-login");
     fetch("/api/user/login", {
       method: "post",
